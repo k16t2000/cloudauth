cbf6e377f77805ed18ff4ed646abb20a0a42337f
k16t2000 <k16t2000@mail.ru>
2021-05-18 18:29:59 +0300

    commit1

--- app/src/main/java/com/example/testcloudauth/CalendarActivity.java
+++ app/src/main/java/com/example/testcloudauth/CalendarActivity.java
@@ -21,50 +21,47 @@ import java.util.List;
 import java.util.Locale;
 
 public class CalendarActivity extends AppCompatActivity {
-    ImageButton NextButton, PreviousButton;
+    ImageButton NextButton,PreviousButton;
     TextView CurrentDate;
     GridView gridView;
-    private static final int MAX_CALENDAR_DAYS = 42;
-    Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    private static final int MAX_CALENDAR_DAYS=42;
+    Calendar calendar=Calendar.getInstance(Locale.ENGLISH);
     Context context;
-    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
-    SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH);
-    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+    SimpleDateFormat dateFormat=new SimpleDateFormat("MMMM yyyy",Locale.ENGLISH);
+    SimpleDateFormat monthFormat=new SimpleDateFormat("MMMM",Locale.ENGLISH);
+    SimpleDateFormat yearFormat=new SimpleDateFormat("yyyy",Locale.ENGLISH);
 
-    List<Date> dates = new ArrayList<>();
-    List<Events> eventsList = new ArrayList<>();
-
+    List<Date> dates=new ArrayList<>();
+    List<Events> eventsList=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        NextButton = findViewById(R.id.nextBtn);
-        PreviousButton = findViewById(R.id.previousBtn);
-        CurrentDate = findViewById(R.id.current_Date);
-        gridView = findViewById(R.id.gridView);
+        NextButton=findViewById(R.id.nextBtn);
+        PreviousButton=findViewById(R.id.previousBtn);
+        CurrentDate=findViewById(R.id.current_Date);
+        gridView=findViewById(R.id.gridView);
 
         PreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                calendar.add(Calendar.MONTH, -1);
+            public void onClick(View view) {
+                calendar.add(Calendar.MONTH,-1);
                 SetUpCalendar();
             }
         });
         NextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                calendar.add(Calendar.MONTH, 1);
+            public void onClick(View view) {
+                calendar.add(Calendar.MONTH,1);
                 SetUpCalendar();
             }
         });
     }
 
-    private void SetUpCalendar() {
-        String currentDate = dateFormat.format(calendar.getTime());
+    private void SetUpCalendar(){
+        String currentDate=dateFormat.format(calendar.getTime());
         CurrentDate.setText(currentDate);
     }
+
 }
-
-
-
