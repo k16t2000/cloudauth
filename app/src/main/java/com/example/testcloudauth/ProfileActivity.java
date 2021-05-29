75502462ad114a333f9976300f4ec71df4a4478f
kebo <bogdan.parubok@hotmail.com>
2021-05-29 23:27:12 +0300

    Changes app icons and adds blank method for profile pic change.

--- app/src/main/java/com/example/testcloudauth/ProfileActivity.java
+++ app/src/main/java/com/example/testcloudauth/ProfileActivity.java
@@ -2,11 +2,15 @@ package com.example.testcloudauth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -166,6 +170,10 @@ public class ProfileActivity extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+
+    public void imageChange(View view){
+
     }
 
     /**
