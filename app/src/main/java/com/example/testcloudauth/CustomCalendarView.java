2075c6ad9eb58cf26f6f354d06ed74d3005d512e
Andrew <graduadu@gmail.com>
2021-06-02 14:46:10 +0300

    Changed timepicker to numberpicker

--- app/src/main/java/com/example/testcloudauth/CustomCalendarView.java
+++ app/src/main/java/com/example/testcloudauth/CustomCalendarView.java
@@ -29,10 +29,12 @@ import java.util.List;
 import java.util.Locale;
 
 public class CustomCalendarView extends LinearLayout {
-    private ImageButton NextButton,PreviousButton, ibSetWorkHors;
-    private Button btnSaveWorkHours, btnSaveNumberpicker;
-    private TextView CurrentDate, tvWorkHours;
+    private ImageButton NextButton,PreviousButton;
+    private Button btnSaveWorkHours;
+    private TextView CurrentDate;
     private GridView gridView;
+
+
     private FirebaseAuth mAuth;
     private FirebaseUser user;
     private String userId;
@@ -46,9 +48,12 @@ public class CustomCalendarView extends LinearLayout {
     SimpleDateFormat workDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
 
     CalendarAdapter calendarAdapter;
-    AlertDialog alertDialogAddWorkTime, alertDialogNumberpicker;
+    AlertDialog alertDialogAddWorkTime;
     List<Date> dates = new ArrayList<>();
     List<WorkingHoursList> workingHoursList = new ArrayList<>();
+
+    private final int MIN_WORK_HOURS = 1;
+    private final int MAX_WORK_HOURS = 24;
 
     public CustomCalendarView(Context context) { super(context); }
 
@@ -79,35 +84,11 @@ public class CustomCalendarView extends LinearLayout {
             public void onItemClick(final AdapterView<?> parent, View view, int position, long id) {
                 final AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 builder.setCancelable(true);
-                final View addView = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_new_work_time_layout, null, false);
-                tvWorkHours = addView.findViewById(R.id.tv_work_hours);
-                ibSetWorkHors = addView.findViewById(R.id.ib_set_work_time);
-                btnSaveWorkHours = addView.findViewById(R.id.btn_save_work_hours);
-
-                // calling TimePickerDialog when clicking on the clock image
-                ibSetWorkHors.setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        AlertDialog.Builder npBuilder = new AlertDialog.Builder(context);
-                        npBuilder.setCancelable(true);
-                        final View npView = LayoutInflater.from(parent.getContext()).inflate(R.layout.numberpicker_layout, null, false);
-                        btnSaveNumberpicker = npView.findViewById(R.id.btn_save_numberpicker);
-                        final NumberPicker numberPicker = (NumberPicker) npView.findViewById(R.id.numberpicker);
-                        numberPicker.setMinValue(1);
-                        numberPicker.setMaxValue(23);
-                        btnSaveNumberpicker.setOnClickListener(new OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                int selectedValue = numberPicker.getValue();
-                                tvWorkHours.setText(Integer.toString(selectedValue));
-                                alertDialogNumberpicker.dismiss();
-                            }
-                        });
-                        npBuilder.setView(npView);
-                        alertDialogNumberpicker = npBuilder.create();
-                        alertDialogNumberpicker.show();
-                    }
-                });
+                final View addView = LayoutInflater.from(parent.getContext()).inflate(R.layout.numberpicker_layout, null, false);
+                btnSaveWorkHours = addView.findViewById(R.id.btn_save_numberpicker);
+                final NumberPicker numberPicker = (NumberPicker) addView.findViewById(R.id.numberpicker);
+                numberPicker.setMinValue(MIN_WORK_HOURS);
+                numberPicker.setMaxValue(MAX_WORK_HOURS);
 
                 final String workDate = workDateFormat.format(dates.get(position));
 
@@ -115,7 +96,7 @@ public class CustomCalendarView extends LinearLayout {
                 btnSaveWorkHours.setOnClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        int workingDuration = Integer.parseInt(tvWorkHours.getText().toString());
+                        int workingDuration = numberPicker.getValue();
                         if (workingDuration > 0) {
                             SaveWorkHours(userId, workDate, workingDuration);
                             SetUpCalendar();
